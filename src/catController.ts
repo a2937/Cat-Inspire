a819bd2a101c18624121d2055533c84356a53e52
Anna <a.rcottrill521@gmail.com>
2025-11-15 10:13:15 -0500

    feat: remove distracting cat gif

--- src/catController.ts
+++ src/catController.ts
@@ -3,9 +3,11 @@
 export const builtInCats = [
   "https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif",
   "https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif",
-	"https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif",
-	"https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExazJwYzRqbmlhZTl2N3RwN3Z0NHZ3eGNjb3ZpaXl0OXJwZXp4Y240bCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/q1MeAPDDMb43K/giphy.gif",
-  "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmpvOTZkcDF3M3ZqeXFuaG9wZnl3bWNwNmllOWExMTRtbXg2Y2IwNSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/nR4L10XlJcSeQ/giphy.gif",
+   "https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif",
+   "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExazJwYzRqbmlhZTl2N3RwN3Z0NHZ3eGNjb3ZpaXl0OXJwZXp4Y240bCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/q1MeAPDDMb43K/giphy.gif",
+  "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZzUzMjM4d2VsOGp2b3Z1dHVyc2ZiZjBmdXdrYXN1cDRtZm94b3piOCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/VbnUQpnihPSIgIXuZv/giphy.gif",
+  "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExNTBnMWI0aWxqc2RtYWlxdTFrMTY1MTFsZGVqY2hpc3RzN2lzb3M1MCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/v6aOjy0Qo1fIA/giphy.gif",
+  "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExdTQ0bGZnYnMycWMwdjBub212OGt1czB5YmdwM3h4bHZ1dTYza3V0cCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/1iu8uG2cjYFZS6wTxv/giphy.gif"
 ];
 
 
